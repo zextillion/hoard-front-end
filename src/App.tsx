0c18cc7c64622e398383b4807e818200edf5e7ad
zxtion <LookASupernova@gmail.com>
2023-03-25 12:05:35 -0700

    Built color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Gold
-        </GridItem>
+        <GridItem area="aside">Gold</GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
