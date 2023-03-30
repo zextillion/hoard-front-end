21c55bd7575aabd58fd6e73db63288ed7175b882
zxtion <LookASupernova@gmail.com>
2023-03-30 16:23:21 -0700

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,18 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`, // 1024 px
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
-        <GenreList />
+        <GridItem paddingX={5}>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
