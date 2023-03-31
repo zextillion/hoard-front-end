412be595291e8ae311329e25fe206e95afdc5ce1
zxtion <LookASupernova@gmail.com>
2023-03-31 12:57:54 -0700

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
   if (!url) {
-    return "";
+    return noImage;
   }
   const target = "media/";
   const index = url.indexOf(target) + target.length;
